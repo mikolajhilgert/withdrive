6d04349b90875e174b458a1446c7243aa7c969ea
Hilgert,Mikolaj M.A <m.hilgert@student.fontys.nl>
2021-10-06 15:11:55 +0200

    fix

--- withdrive-backend/src/main/java/nl/fontys/withdrive/dto/Trip.java
+++ withdrive-backend/src/main/java/nl/fontys/withdrive/dto/Trip.java
@@ -1,19 +1,11 @@
 package nl.fontys.withdrive.dto;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import java.util.UUID;
 
-//@Entity
-//@NoArgsConstructor
 public abstract class Trip {
-//    @Id
-//    @GeneratedValue
     protected @Getter @Setter
     UUID tripID;
     protected @Getter @Setter String origin;

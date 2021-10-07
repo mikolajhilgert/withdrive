60482af505078528181325088d9900ab0937e210
Hilgert,Mikolaj M.A <m.hilgert@student.fontys.nl>
2021-10-07 18:30:30 +0200

    Added initial tests for trips (backend). Polished documents and files for sprint 2 submission

--- withdrive-backend/src/test/java/nl/fontys/withdrive/TripTests.java
+++ withdrive-backend/src/test/java/nl/fontys/withdrive/TripTests.java
@@ -11,9 +11,7 @@ import nl.fontys.withdrive.services.UserManager;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 public class TripTests {
     @Test
@@ -32,6 +30,51 @@ public class TripTests {
     }
 
     @Test
+    public void TestCreateTripThenExpectCorrectDetails(){
+        //Arrange
+        IUserData data = new FakeUserData();
+        TripManager trips = new TripManager(new FakeTripData(),data);
+        UUID driver = data.RetrieveAll().get(0).getClientNumber();
+        UUID temp = UUID.randomUUID();
+
+        //Act
+        trips.Add(new TripVM(temp, "og","og","og",driver,Collections.emptyList()));
+        TripDTO result = trips.RetrieveByNumber(temp);
+
+
+        //Assert
+        Assertions.assertEquals(temp,result.getTripID());
+        Assertions.assertEquals("og",result.getOrigin());
+        Assertions.assertEquals("og",result.getDestination());
+        Assertions.assertEquals("og",result.getDescription());
+        Assertions.assertEquals(data.RetrieveByNumber(driver),result.getDriver());
+        Assertions.assertTrue(result.getPassengers().isEmpty());
+    }
+
+    @Test
+    public void TestEditTripThenExpectEditedDetails(){
+        //Arrange
+        IUserData data = new FakeUserData();
+        TripManager trips = new TripManager(new FakeTripData(),data);
+        UUID driver = data.RetrieveAll().get(0).getClientNumber();
+        UUID temp = UUID.randomUUID();
+
+        //Act
+        trips.Add(new TripVM(temp, "og","og","og",driver,Collections.emptyList()));
+        trips.Update(new TripVM(temp, "test","test","test",driver,Collections.emptyList()));
+        TripDTO result = trips.RetrieveByNumber(temp);
+
+
+        //Assert
+        Assertions.assertEquals(temp,result.getTripID());
+        Assertions.assertEquals("test",result.getOrigin());
+        Assertions.assertEquals("test",result.getDestination());
+        Assertions.assertEquals("test",result.getDescription());
+        Assertions.assertEquals(data.RetrieveByNumber(driver),result.getDriver());
+        Assertions.assertTrue(result.getPassengers().isEmpty());
+    }
+
+    @Test
     public void TestAddTripThenDeleteTripExpectUserWasDeleted(){
         //Arrange
         IUserData data = new FakeUserData();
@@ -47,8 +90,14 @@ public class TripTests {
         Assertions.assertNull(result);
     }
 
-//    @Test
-//    public void TestEditTripDetails(){
-//
-//    }
+    @Test
+    public void TestRetrievalOfNonExistingTrip(){
+        //Arrange
+        TripManager trips = new TripManager(new FakeTripData(),new FakeUserData());
+        //Act
+        TripDTO actual = trips.RetrieveByNumber(UUID.randomUUID());
+        //Assert
+        Assertions.assertNull(actual);
+    }
+
 }

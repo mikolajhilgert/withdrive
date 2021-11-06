b2b67f070a692725e151714d1b9d143412394090
Hilgert,Mikolaj M.A <m.hilgert@student.fontys.nl>
2021-11-06 21:33:33 +0100

    added trip service tests

--- withdrive-backend/src/test/java/nl/fontys/withdrive/TripServiceTest.java
+++ withdrive-backend/src/test/java/nl/fontys/withdrive/TripServiceTest.java
@@ -1,11 +1,15 @@
 package nl.fontys.withdrive;
 
+import lombok.Getter;
+import lombok.Setter;
 import nl.fontys.withdrive.converter.TripConverter;
 import nl.fontys.withdrive.converter.UserConverter;
+import nl.fontys.withdrive.dto.trip.TripRequestDTO;
 import nl.fontys.withdrive.dto.trip.TripResponseDTO;
 import nl.fontys.withdrive.dto.user.UserDTO;
 import nl.fontys.withdrive.entity.Trip;
 import nl.fontys.withdrive.entity.TripApplication;
+import nl.fontys.withdrive.entity.TripApplicationKEY;
 import nl.fontys.withdrive.entity.User;
 import nl.fontys.withdrive.enumeration.ApplicationStatus;
 import nl.fontys.withdrive.enumeration.TripStatus;
@@ -27,6 +31,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -40,34 +45,78 @@ public class TripServiceTest {
     // https://javacodehouse.com/blog/mockito-tutorial/
     @Mock
     ITripData db;
+    @Mock
     IUserData userData;
+    @Mock
     IApplicationData appData;
+
     ITripManager service;
-    ITripConverter converter = new TripConverter(new ModelMapper(),userData,appData);
-    UUID testID = UUID.randomUUID();
-    UUID testID2 = UUID.randomUUID();
-    UUID testID3 = UUID.randomUUID();
+    ITripConverter converter;
+    UUID tripID = UUID.randomUUID();
+    UUID tripID2 = UUID.randomUUID();
+    UUID driverID = UUID.randomUUID();
+    UUID userID = UUID.randomUUID();
 
     @BeforeEach
     public void setUp()  {
+        converter = new TripConverter(new ModelMapper(),userData,appData);
         service = new TripManager(db,userData,converter);
-    }
+        User driver = new User(driverID,"john.doe@gmail.com","John","Doe","10-02-1990","Male","789762183","password",null,null);
+        User user = new User(userID,"john.doe@gmail.com","John","Doe","10-02-1990","Male","789762183","password",null,null);
+        List<Trip> trips = List.of(
+                new Trip(tripID,"Eindhoven","Venlo","Quick trip", TripStatus.AWAITING,null,driver),
+                new Trip(tripID2,"Venlo","Eindhoven","Quick trip", TripStatus.AWAITING,null,driver)
+        );
+        TripApplication tripApplication = new TripApplication(new TripApplicationKEY(),user,trips.get(0),ApplicationStatus.PENDING,"test");
 
-    //Work in progress
-//    @Test
-//    public void getAllTripsTest()
-//    {
-//        User driver = new User(testID2,"john.doe@gmail.com","John","Doe","10-02-1990","Male","789762183","password",null,null);
-//        User user = new User(testID3,"john.doe@gmail.com","John","Doe","10-02-1990","Male","789762183","password",null,null);
-//        Trip trip = new Trip(testID,"Eindhoven","Venlo","lol", TripStatus.AWAITING,null,driver);
-//
-//        when(db.RetrieveByNumber(testID)).thenReturn(trip);
-//
-//        TripResponseDTO toCheck = service.RetrieveByNumber(testID);
-//
-//        System.out.println(testID);
-//        System.out.println(toCheck.getTripID());
-//
-//        Assertions.assertEquals(toCheck.getTripID(), testID);
-//    }
+        when(db.RetrieveByNumber(tripID)).thenReturn(trips.get(0));
+        when(db.RetrieveAll()).thenReturn(trips);
+        when(userData.RetrieveByID(driverID)).thenReturn(driver);
+        when(userData.RetrieveByID(userID)).thenReturn(user);
+    }
+    @Test
+    public void getAllTripsTest()
+    {
+        List<TripResponseDTO> output = service.RetrieveAll();
+
+        Assertions.assertEquals(output.size(),2);
+        Assertions.assertEquals(output.get(0).getTripID(), tripID);
+        Assertions.assertEquals(output.get(1).getTripID(), tripID2);
+    }
+    @Test
+    public void getTripsByIDTest()
+    {
+        TripResponseDTO toCheck = service.RetrieveByNumber(tripID);
+
+        Assertions.assertEquals(toCheck.getTripID(), tripID);
+    }
+    @Test
+    public void addTripTest(){
+        TripRequestDTO trip = new TripRequestDTO(UUID.randomUUID(),"Eindhoven","Venlo","Quick trip",userData.RetrieveByID(driverID).getUserID(),new ArrayList<>(), TripStatus.AWAITING);
+        service.Add(trip);
+
+        ArgumentCaptor<Trip> UserArgumentCaptor = ArgumentCaptor.forClass(Trip.class);
+        verify(db).Create(UserArgumentCaptor.capture());
+        Trip finalResult = UserArgumentCaptor.getValue();
+        Assertions.assertEquals(finalResult.getTripID(),trip.getTripID());
+    }
+    @Test
+    public void editTripTest(){
+        Trip trip = db.RetrieveByNumber(tripID);
+        trip.setOrigin("test");
+        service.Update(new TripRequestDTO(trip.getTripID(),trip.getOrigin(),trip.getDestination(),trip.getDescription(),userData.RetrieveByID(driverID).getUserID(), new ArrayList<>(),trip.getStatus()));
+
+        ArgumentCaptor<Trip> UserArgumentCaptor = ArgumentCaptor.forClass(Trip.class);
+        verify(db).Create(UserArgumentCaptor.capture());
+        Trip finalResult = UserArgumentCaptor.getValue();
+        Assertions.assertEquals(finalResult.getTripID(),trip.getTripID());
+        Assertions.assertEquals(finalResult.getOrigin(),trip.getOrigin());
+    }
+    @Test
+    public void deleteTripByIDTest()
+    {
+        service.Delete(tripID);
+
+        verify(db).Delete(tripID);
+    }
 }

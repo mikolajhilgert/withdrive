952ae6daba8a6511964c91a626532abaf78c9712
Hilgert,Mikolaj M.A <m.hilgert@student.fontys.nl>
2022-01-14 15:42:12 +0100

    fix

--- withdrive-frontend/cypress/integration/3edit-details.js
+++ withdrive-frontend/cypress/integration/3edit-details.js
@@ -1,10 +1,8 @@
 describe('editdetails', () => {
     it("edit_details", () => {
         cy.login("drivertester@withdrive.com","password")
-        cy.visit('/')
+        cy.visit('/my-profile')
         cy.wait(1000)
-        /* ==== Generated with Cypress Studio ==== */
-        cy.get('[href="/my-profile"]').click();
         cy.get('.innerPage_authwrapper__36oWg').click();
         cy.get(':nth-child(4) > .form-control').clear();
         cy.get(':nth-child(4) > .form-control').type('Black');
